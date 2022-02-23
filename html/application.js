85daf801d39362acbd019487db44cc33a1a60048
Jakob Skjerning <jakob@mentalized.net>
2022-02-23 13:33:54 +0100

    Draw a sample graph

--- html/application.js
+++ html/application.js
@@ -1,1 +1,44 @@
-console.log('Hello world?')
+import cytoscape from "cytoscape/dist/cytoscape.esm";
+
+const elements = [
+  { // node a
+    data: { id: "a" }
+  },
+  { // node b
+    data: { id: "b" }
+  },
+  { // edge ab
+    data: { id: "ab", source: "a", target: "b" }
+  }
+]
+const layout = {
+  name: "grid",
+  rows: 1
+}
+const style = [ // the stylesheet for the graph
+  {
+    selector: "node",
+    style: {
+      "background-color": "#666",
+      "label": "data(id)"
+    }
+  },
+
+  {
+    selector: "edge",
+    style: {
+      "curve-style": "bezier",
+      "line-color": "#ccc",
+      "target-arrow-color": "#ccc",
+      "target-arrow-shape": "triangle",
+      "width": 3
+    }
+  }
+]
+
+var graph = cytoscape({
+  container: document.getElementById("graph"),
+  elements: elements,
+  layout: layout,
+  style: style,
+});
