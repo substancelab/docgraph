c63f890c984500e78e7598e8dcc8846a561a7304
Jakob Skjerning <jakob@mentalized.net>
2022-02-25 11:12:56 +0100

    Load elements from a separate file
    Now it's up to the generator to generate this file.

--- html/application.js
+++ html/application.js
@@ -2,49 +2,8 @@ import cytoscape from "cytoscape/dist/cytoscape.esm";
 import dagre from 'cytoscape-dagre';
 cytoscape.use(dagre)
 
-const elements = [
-  { // group 01
-    data: { id: "01", classes: ["group"] }
-  },
-  { // group 02
-    data: { id: "02", classes: ["group"] }
-  },
-  { // group 03
-    data: { id: "03", classes: ["group"] }
-  },
-  { // group 04
-    data: { id: "04", classes: ["group"] }
-  },
-  { // node a
-    data: { id: "a", parent: "01" }
-  },
-  { // node b
-    data: { id: "b", parent: "02" }
-  },
-  { // node c
-    data: { id: "c", parent: "03" }
-  },
-  { // node d
-    data: { id: "d", parent: "04" }
-  },
-  { // node e
-    data: { id: "e", parent: "04" }
-  },
-  { // edge ab
-    data: { id: "ab", source: "a", target: "b" }
-  },
-  { // edge ac
-    data: { id: "ac", source: "a", target: "c" }
-  },
-  { // edge bd
-    data: { id: "bd", source: "b", target: "d" }
-  },
-  { // edge ae
-    data: { id: "ae", source: "a", target: "e" }
-  }
+import elements from "./elements"
 
-
-]
 const layout = {
   name: "dagre"
 }
---
+++ html/elements.js
@@ -0,0 +1,43 @@
+const elements = [
+  { // group 01
+    data: { id: "01", classes: ["group"] }
+  },
+  { // group 02
+    data: { id: "02", classes: ["group"] }
+  },
+  { // group 03
+    data: { id: "03", classes: ["group"] }
+  },
+  { // group 04
+    data: { id: "04", classes: ["group"] }
+  },
+  { // node a
+    data: { id: "a", parent: "01" }
+  },
+  { // node b
+    data: { id: "b", parent: "02" }
+  },
+  { // node c
+    data: { id: "c", parent: "03" }
+  },
+  { // node d
+    data: { id: "d", parent: "04" }
+  },
+  { // node e
+    data: { id: "e", parent: "04" }
+  },
+  { // edge ab
+    data: { id: "ab", source: "a", target: "b" }
+  },
+  { // edge ac
+    data: { id: "ac", source: "a", target: "c" }
+  },
+  { // edge bd
+    data: { id: "bd", source: "b", target: "d" }
+  },
+  { // edge ae
+    data: { id: "ae", source: "a", target: "e" }
+  }
+]
+
+export default elements
