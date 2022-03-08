d9037518b8c395d4c7b497297c47115ba37e36a5
Jakob Skjerning <jakob@mentalized.net>
2022-03-08 09:37:18 +0100

    Apply standard formatting

--- html/application.js
+++ html/application.js
@@ -1,94 +1,94 @@
-import * as d3 from "d3"
-import * as webcola from "webcola"
+import * as d3 from 'd3'
+import * as webcola from 'webcola'
 
-function layoutWithHierarchicalGrouping(graph, svg) {
-  var color = d3.scaleLinear().domain([1,10]).range(["green", "blue"])
-  var cola = webcola.d3adaptor(d3)
-  .linkDistance(80)
-  .avoidOverlaps(true)
-  .handleDisconnected(false)
-  .size([width, height]);
+function layoutWithHierarchicalGrouping (graph, svg) {
+  const color = d3.scaleLinear().domain([1, 10]).range(['green', 'blue'])
+  const cola = webcola.d3adaptor(d3)
+    .linkDistance(80)
+    .avoidOverlaps(true)
+    .handleDisconnected(false)
+    .size([width, height])
 
   graph.nodes.forEach(function (v) {
-      v.width = v.height = 95;
+    v.width = v.height = 95
   })
 
-    const groups = graph.groups || []
-    groups.forEach(function (g) { g.padding = 0.01; });
-    cola
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .groups(groups)
-      .start(100, 0, 50, 50);
+  const groups = graph.groups || []
+  groups.forEach(function (g) { g.padding = 0.01 })
+  cola
+    .nodes(graph.nodes)
+    .links(graph.links)
+    .groups(groups)
+    .start(100, 0, 50, 50)
 
-  var group = svg.selectAll(".group")
-      .data(groups)
-    .enter().append("rect")
-      .attr("rx", 8).attr("ry", 8)
-      .attr("class", "group")
-      .style("fill", function (d, i) { return color(i); });
+  const group = svg.selectAll('.group')
+    .data(groups)
+    .enter().append('rect')
+    .attr('rx', 8).attr('ry', 8)
+    .attr('class', 'group')
+    .style('fill', function (d, i) { return color(i) })
 
-  var link = svg.selectAll(".link")
-      .data(graph.links)
-    .enter().append("line")
-      .attr("class", "link");
+  const link = svg.selectAll('.link')
+    .data(graph.links)
+    .enter().append('line')
+    .attr('class', 'link')
 
-  var pad = 20;
-  var node = svg.selectAll(".node")
-      .data(graph.nodes)
-    .enter().append("rect")
-      .attr("class", "node")
-      .attr("width", function (d) { return d.width - 2 * pad; })
-      .attr("height", function (d) { return d.height - 2 * pad; })
-      .attr("rx", 5).attr("ry", 5)
-      .style("fill", function (d) { return color(groups.length); })
-      .call(cola.drag)
-      .on('mouseup', function (d) {
-          d.fixed = 0;
-          cola.alpha(1); // fire it off again to satify gridify
-      });
+  const pad = 20
+  const node = svg.selectAll('.node')
+    .data(graph.nodes)
+    .enter().append('rect')
+    .attr('class', 'node')
+    .attr('width', function (d) { return d.width - 2 * pad })
+    .attr('height', function (d) { return d.height - 2 * pad })
+    .attr('rx', 5).attr('ry', 5)
+    .style('fill', function (d) { return color(groups.length) })
+    .call(cola.drag)
+    .on('mouseup', function (d) {
+      d.fixed = 0
+      cola.alpha(1) // fire it off again to satify gridify
+    })
 
-  var label = svg.selectAll(".label")
-      .data(graph.nodes)
-     .enter().append("text")
-      .attr("class", "label")
-      .text(function (d) { return d.name; })
-      .call(cola.drag);
+  const label = svg.selectAll('.label')
+    .data(graph.nodes)
+    .enter().append('text')
+    .attr('class', 'label')
+    .text(function (d) { return d.name })
+    .call(cola.drag)
 
-  node.append("title")
-      .text(function (d) { return d.name; });
+  node.append('title')
+    .text(function (d) { return d.name })
 
-  cola.on("tick", function () {
-      link.attr("x1", function (d) { return d.source.x; })
-          .attr("y1", function (d) { return d.source.y; })
-          .attr("x2", function (d) { return d.target.x; })
-          .attr("y2", function (d) { return d.target.y; });
+  cola.on('tick', function () {
+    link.attr('x1', function (d) { return d.source.x })
+      .attr('y1', function (d) { return d.source.y })
+      .attr('x2', function (d) { return d.target.x })
+      .attr('y2', function (d) { return d.target.y })
 
-      node.attr("x", function (d) { return d.x - d.width / 2 + pad; })
-          .attr("y", function (d) { return d.y - d.height / 2 + pad; });
+    node.attr('x', function (d) { return d.x - d.width / 2 + pad })
+      .attr('y', function (d) { return d.y - d.height / 2 + pad })
 
-      group.attr("x", function (d) { return d.bounds.x; })
-           .attr("y", function (d) { return d.bounds.y; })
-          .attr("width", function (d) { return d.bounds.width(); })
-          .attr("height", function (d) { return d.bounds.height(); });
+    group.attr('x', function (d) { return d.bounds.x })
+      .attr('y', function (d) { return d.bounds.y })
+      .attr('width', function (d) { return d.bounds.width() })
+      .attr('height', function (d) { return d.bounds.height() })
 
-      label.attr("x", function (d) { return d.x; })
-           .attr("y", function (d) {
-               var h = this.getBBox().height;
-               return d.y + h/4;
-           });
-  });
+    label.attr('x', function (d) { return d.x })
+      .attr('y', function (d) {
+        const h = this.getBBox().height
+        return d.y + h / 4
+      })
+  })
 }
 
-var width = 960, height = 500;
+var width = 960; var height = 500
 
-var svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
+const svg = d3
+  .select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
 
 // Load datad an generate graph
-d3.json("./elements.json").then((graph) => {
+d3.json('./elements.json').then((graph) => {
   layoutWithHierarchicalGrouping(graph, svg)
-});
+})
