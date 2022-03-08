22473cd45adf26588e663e4505008ee085d21e2e
Jakob Skjerning <jakob@mentalized.net>
2022-03-08 09:12:24 +0100

    Use example
    From https://ialab.it.monash.edu/webcola/examples/gridifiedSmallGroups.html

--- html/application.js
+++ html/application.js
@@ -1,66 +1,86 @@
-import cytoscape from "cytoscape/dist/cytoscape.esm";
-import dagre from 'cytoscape-dagre';
-cytoscape.use(dagre)
+import * as d3 from "d3"
 
-import elements from "./elements"
+var width = 960, height = 500;
 
-const layout = {
-  name: "dagre",
+var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-  // Labels should be included in determining the space used by a node
-  nodeDimensionsIncludeLabels: true,
+var cola = cola.d3adaptor(d3)
+.linkDistance(80)
+.avoidOverlaps(true)
+.handleDisconnected(false)
+.size([width, height]);
 
-  nodeSep: 100,
+var svg = d3.select("body").append("svg")
+.attr("width", width)
+.attr("height", height);
 
-  // Flow ranks from top to bottom
-  rankDir: "TB",
+d3.json("graphdata/smallgrouped.json", function (error, graph) {
 
-  rankSep: 100,
-}
+graph.nodes.forEach(function (v) {
+    v.width = v.height = 95;
+})
+graph.groups.forEach(function (g) { g.padding = 0.01; });
+cola
+    .nodes(graph.nodes)
+    .links(graph.links)
+    .groups(graph.groups)
+    .start(100, 0, 50, 50);
 
-// the stylesheet for the graph
-const style = [
-  {
-    selector: "node",
-    style: {
-      "label": "data(title)"
-    }
-  },
+var group = svg.selectAll(".group")
+    .data(graph.groups)
+  .enter().append("rect")
+    .attr("rx", 8).attr("ry", 8)
+    .attr("class", "group")
+    .style("fill", function (d, i) { return color(i); });
 
-  {
-    selector: 'node:parent',
-    css: {
-      'background-opacity': 0.333,
-      "label": "data(id)",
-    }
-  },
+var link = svg.selectAll(".link")
+    .data(graph.links)
+  .enter().append("line")
+    .attr("class", "link");
 
-  {
-    selector: "node.group",
-    style: {
-      "label": "data(id)"
-    }
-  },
+var pad = 20;
+var node = svg.selectAll(".node")
+    .data(graph.nodes)
+  .enter().append("rect")
+    .attr("class", "node")
+    .attr("width", function (d) { return d.width - 2 * pad; })
+    .attr("height", function (d) { return d.height - 2 * pad; })
+    .attr("rx", 5).attr("ry", 5)
+    .style("fill", function (d) { return color(graph.groups.length); })
+    .call(cola.drag)
+    .on('mouseup', function (d) {
+        d.fixed = 0;
+        cola.alpha(1); // fire it off again to satify gridify
+    });
 
-  {
-    selector: "edge",
-    style: {
-      "curve-style": "bezier",
-      "line-color": "#ccc",
-      "target-arrow-color": "#ccc",
-      "target-arrow-shape": "triangle",
-      "width": 2
-    }
-  }
-]
+var label = svg.selectAll(".label")
+    .data(graph.nodes)
+   .enter().append("text")
+    .attr("class", "label")
+    .text(function (d) { return d.name; })
+    .call(cola.drag);
 
-var graph = cytoscape({
-  autoungrabify: true,
-  autounselectify: true,
-  container: document.getElementById("graph"),
-  elements: elements,
-  layout: layout,
-  style: style,
-  userPanningEnabled: false,
-  userZoomingEnabled: false
+node.append("title")
+    .text(function (d) { return d.name; });
+
+cola.on("tick", function () {
+    link.attr("x1", function (d) { return d.source.x; })
+        .attr("y1", function (d) { return d.source.y; })
+        .attr("x2", function (d) { return d.target.x; })
+        .attr("y2", function (d) { return d.target.y; });
+
+    node.attr("x", function (d) { return d.x - d.width / 2 + pad; })
+        .attr("y", function (d) { return d.y - d.height / 2 + pad; });
+
+    group.attr("x", function (d) { return d.bounds.x; })
+         .attr("y", function (d) { return d.bounds.y; })
+        .attr("width", function (d) { return d.bounds.width(); })
+        .attr("height", function (d) { return d.bounds.height(); });
+
+    label.attr("x", function (d) { return d.x; })
+         .attr("y", function (d) {
+             var h = this.getBBox().height;
+             return d.y + h/4;
+         });
+});
 });
