57bb68067d5e5b469f72e44d802309942d9d3e84
Jakob Skjerning <jakob@mentalized.net>
2022-03-08 10:38:12 +0100

    Remove simple groups example
    It wasn't workable for us

--- html/application.js
+++ html/application.js
@@ -83,78 +83,6 @@ function layoutWithHierarchicalGrouping (graph, svg) {
   })
 }
 
-function simpleGroupsExample (graph, svg) {
-  const color = d3.scaleLinear().domain([1, 10]).range(['green', 'blue'])
-
-  var cola = webcola.d3adaptor(d3)
-    .size([width, height])
-
-  const groupMap = {}
-  graph.nodes.forEach(function (v, i) {
-    const g = v.group
-    if (typeof groupMap[g] === 'undefined') {
-      groupMap[g] = []
-    }
-    groupMap[g].push(i)
-
-    v.width = v.height = 10
-  })
-
-  const groups = []
-  for (const g in groupMap) {
-    groups.push({ id: g, leaves: groupMap[g] })
-  }
-  cola
-    .nodes(graph.nodes)
-    .links(graph.links)
-    .groups(groups)
-    .jaccardLinkLengths(40, 0.7)
-    .avoidOverlaps(true)
-    .start(50, 0, 50)
-
-  const group = svg.selectAll('.group')
-    .data(groups)
-    .enter().append('rect')
-    .classed('group', true)
-    .attr('rx', 5)
-    .attr('ry', 5)
-    .style('fill', function (d) { return color(d.id) })
-    .call(cola.drag)
-
-  const link = svg.selectAll('.link')
-    .data(graph.links)
-    .enter().append('line')
-    .attr('class', 'link')
-    .style('stroke-width', function (d) { return Math.sqrt(d.value) })
-
-  const node = svg.selectAll('.node')
-    .data(graph.nodes)
-    .enter().append('circle')
-    .attr('class', 'node')
-    .attr('r', 5)
-    .style('fill', function (d) { return color(d.group) })
-    .call(cola.drag)
-
-  node.append('title')
-    .text(function (d) { return d.name })
-
-  cola.on('tick', function () {
-    link.attr('x1', function (d) { return d.source.x })
-      .attr('y1', function (d) { return d.source.y })
-      .attr('x2', function (d) { return d.target.x })
-      .attr('y2', function (d) { return d.target.y })
-
-    node.attr('cx', function (d) { return d.x })
-      .attr('cy', function (d) { return d.y })
-
-    group
-      .attr('x', function (d) { return d.bounds.x })
-      .attr('y', function (d) { return d.bounds.y })
-      .attr('width', function (d) { return d.bounds.width() })
-      .attr('height', function (d) { return d.bounds.height() })
-  })
-}
-
 var width = 960; var height = 1500
 
 const layoutWithHierarchicalGroupingSvg = d3
@@ -163,15 +91,6 @@ const layoutWithHierarchicalGroupingSvg = d3
   .attr('width', width)
   .attr('height', height)
 
-const simpleGroupsExampleSvg = d3
-  .select('body')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
-
-
-  // Load data and generate graph
 d3.json('./elements.json').then((data) => {
-  // layoutWithHierarchicalGrouping(data, layoutWithHierarchicalGroupingSvg)
-  simpleGroupsExample(data, simpleGroupsExampleSvg)
+  layoutWithHierarchicalGrouping(data, layoutWithHierarchicalGroupingSvg)
 })
