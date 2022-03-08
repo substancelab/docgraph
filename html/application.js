a36ab6f3d7ca91093cb6a12e7aea14e4bbe769d0
Jakob Skjerning <jakob@mentalized.net>
2022-03-08 10:02:31 +0100

    Upgrade for D3 v7

--- html/application.js
+++ html/application.js
@@ -81,95 +81,94 @@ function layoutWithHierarchicalGrouping (graph, svg) {
 }
 
 function simpleGroupsExample (graph, svg) {
-  const width = 960
-  const height = 500
+  const color = d3.scaleLinear().domain([1, 10]).range(['green', 'blue'])
 
-  const color = d3.scaleOrdinal(d3.schemeCategory20)
-
-  var cola = cola.d3adaptor(d3)
+  var cola = webcola.d3adaptor(d3)
     .size([width, height])
 
-  var svg = d3.select('body').append('svg')
-    .attr('width', width)
-    .attr('height', height)
-
-  d3.json('graphdata/miserables.json', function (error, graph) {
-    const groupMap = {}
-    graph.nodes.forEach(function (v, i) {
-      const g = v.group
-      if (typeof groupMap[g] === 'undefined') {
-        groupMap[g] = []
-      }
-      groupMap[g].push(i)
-
-      v.width = v.height = 10
-    })
-
-    const groups = []
-    for (const g in groupMap) {
-      groups.push({ id: g, leaves: groupMap[g] })
+  const groupMap = {}
+  graph.nodes.forEach(function (v, i) {
+    const g = v.group
+    if (typeof groupMap[g] === 'undefined') {
+      groupMap[g] = []
     }
-    cola
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .groups(groups)
-      .jaccardLinkLengths(40, 0.7)
-      .avoidOverlaps(true)
-      .start(50, 0, 50)
+    groupMap[g].push(i)
 
-    const group = svg.selectAll('.group')
-      .data(groups)
-      .enter().append('rect')
-      .classed('group', true)
-      .attr('rx', 5)
-      .attr('ry', 5)
-      .style('fill', function (d) { return color(d.id) })
-      .call(cola.drag)
+    v.width = v.height = 10
+  })
 
-    const link = svg.selectAll('.link')
-      .data(graph.links)
-      .enter().append('line')
-      .attr('class', 'link')
-      .style('stroke-width', function (d) { return Math.sqrt(d.value) })
+  const groups = []
+  for (const g in groupMap) {
+    groups.push({ id: g, leaves: groupMap[g] })
+  }
+  cola
+    .nodes(graph.nodes)
+    .links(graph.links)
+    .groups(groups)
+    .jaccardLinkLengths(40, 0.7)
+    .avoidOverlaps(true)
+    .start(50, 0, 50)
 
-    const node = svg.selectAll('.node')
-      .data(graph.nodes)
-      .enter().append('circle')
-      .attr('class', 'node')
-      .attr('r', 5)
-      .style('fill', function (d) { return color(d.group) })
-      .call(cola.drag)
+  const group = svg.selectAll('.group')
+    .data(groups)
+    .enter().append('rect')
+    .classed('group', true)
+    .attr('rx', 5)
+    .attr('ry', 5)
+    .style('fill', function (d) { return color(d.id) })
+    .call(cola.drag)
 
-    node.append('title')
-      .text(function (d) { return d.name })
+  const link = svg.selectAll('.link')
+    .data(graph.links)
+    .enter().append('line')
+    .attr('class', 'link')
+    .style('stroke-width', function (d) { return Math.sqrt(d.value) })
 
-    cola.on('tick', function () {
-      link.attr('x1', function (d) { return d.source.x })
-        .attr('y1', function (d) { return d.source.y })
-        .attr('x2', function (d) { return d.target.x })
-        .attr('y2', function (d) { return d.target.y })
+  const node = svg.selectAll('.node')
+    .data(graph.nodes)
+    .enter().append('circle')
+    .attr('class', 'node')
+    .attr('r', 5)
+    .style('fill', function (d) { return color(d.group) })
+    .call(cola.drag)
 
-      node.attr('cx', function (d) { return d.x })
-        .attr('cy', function (d) { return d.y })
+  node.append('title')
+    .text(function (d) { return d.name })
 
-      group
-        .attr('x', function (d) { return d.bounds.x })
-        .attr('y', function (d) { return d.bounds.y })
-        .attr('width', function (d) { return d.bounds.width() })
-        .attr('height', function (d) { return d.bounds.height() })
-    })
+  cola.on('tick', function () {
+    link.attr('x1', function (d) { return d.source.x })
+      .attr('y1', function (d) { return d.source.y })
+      .attr('x2', function (d) { return d.target.x })
+      .attr('y2', function (d) { return d.target.y })
+
+    node.attr('cx', function (d) { return d.x })
+      .attr('cy', function (d) { return d.y })
+
+    group
+      .attr('x', function (d) { return d.bounds.x })
+      .attr('y', function (d) { return d.bounds.y })
+      .attr('width', function (d) { return d.bounds.width() })
+      .attr('height', function (d) { return d.bounds.height() })
   })
 }
 
 var width = 960; var height = 500
 
-const svg = d3
+const layoutWithHierarchicalGroupingSvg = d3
   .select('body')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
 
-// Load datad an generate graph
-d3.json('./elements.json').then((graph) => {
-  layoutWithHierarchicalGrouping(graph, svg)
+const simpleGroupsExampleSvg = d3
+  .select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+
+
+  // Load data and generate graph
+d3.json('./elements.json').then((data) => {
+  // layoutWithHierarchicalGrouping(data, layoutWithHierarchicalGroupingSvg)
+  simpleGroupsExample(data, simpleGroupsExampleSvg)
 })
